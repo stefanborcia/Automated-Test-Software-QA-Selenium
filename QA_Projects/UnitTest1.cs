2a6e5878f68a86339f838be94653285fc7560e75
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-04-23 16:39:59 +0200

    filled automated the text in create an account and click on create

--- QA_Projects/UnitTest1.cs
+++ QA_Projects/UnitTest1.cs
@@ -25,5 +25,26 @@ namespace QA_Projects_Test_Magento
 
             driver.FindElement(By.XPath("//span[@class = 'more button']")).Click();
         }
+
+        [Test]
+        public void FilledInWithTextAndCreateAnAccount()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://magento.softwaretestingboard.com");
+            Thread.Sleep(2000);
+
+            driver.FindElement(By.XPath("//a[contains(text(), 'Create an Account')][1]")).Click();
+            Thread.Sleep(3000);
+
+            driver.FindElement(By.XPath("//input[@id='firstname']")).SendKeys("Stefan");
+            driver.FindElement(By.XPath("//input[@id='lastname']")).SendKeys("Iulian");
+            driver.FindElement(By.XPath("//input[@id='email_address']")).SendKeys("stefan@gmail.com");
+            driver.FindElement(By.XPath("//input[@id='password']")).SendKeys("Password123*");
+            driver.FindElement(By.XPath("//input[@id='password-confirmation']")).SendKeys("Password123*");
+            driver.FindElement(By.XPath("(//button[@type='submit'])[2]")).Click();
+            Thread.Sleep(3000);
+        }
     }
 }
