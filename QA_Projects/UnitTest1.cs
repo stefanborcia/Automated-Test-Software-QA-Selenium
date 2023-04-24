e63e50815326b7d86ac47a647e38d1df0cb0049d
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-04-24 20:09:31 +0200

    create another test to sign in and add item to cart

--- QA_Projects/UnitTest1.cs
+++ QA_Projects/UnitTest1.cs
@@ -20,7 +20,7 @@ namespace QA_Projects_Test_Magento
             driver.Navigate().GoToUrl("https://magento.softwaretestingboard.com");
             Thread.Sleep(2000); //Time for loading the website
 
-            driver.FindElement(By.XPath("//a[@id='ui-id-3']")).Click();
+            driver.FindElement(By.Id("ui-id-3")).Click();
             Thread.Sleep(2000); //Time for loading the website.
 
             driver.FindElement(By.XPath("//span[@class = 'more button']")).Click();
@@ -38,11 +38,12 @@ namespace QA_Projects_Test_Magento
             driver.FindElement(By.XPath("//a[contains(text(), 'Create an Account')][1]")).Click();
             Thread.Sleep(3000);
 
-            driver.FindElement(By.XPath("//input[@id='firstname']")).SendKeys("Stefan");
-            driver.FindElement(By.XPath("//input[@id='lastname']")).SendKeys("Iulian");
-            driver.FindElement(By.XPath("//input[@id='email_address']")).SendKeys("stefan@gmail.com");
-            driver.FindElement(By.XPath("//input[@id='password']")).SendKeys("Password123*");
-            driver.FindElement(By.XPath("//input[@id='password-confirmation']")).SendKeys("Password123*");
+            driver.FindElement(By.Id("firstname")).SendKeys("Stefan"); 
+            driver.FindElement(By.Id("lastname")).SendKeys("Iulian");
+            driver.FindElement(By.Id("is_subscribed")).Click();   // Check Button
+            driver.FindElement(By.Id("email_address")).SendKeys("stefan121@gmail.com");
+            driver.FindElement(By.Id("password")).SendKeys("Password123*");
+            driver.FindElement(By.Id("password-confirmation")).SendKeys("Password123*");
             driver.FindElement(By.XPath("(//button[@type='submit'])[2]")).Click();
             Thread.Sleep(3000);
 
@@ -50,6 +51,30 @@ namespace QA_Projects_Test_Magento
             string actualMessage = driver.FindElement(By.XPath("//div[contains(text(), 'Thank you' )]")).Text;
 
             Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [Test]
+        public void SignInAndAddProductToCard()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://magento.softwaretestingboard.com");
+            Thread.Sleep(2000);
+
+            driver.FindElement(By.XPath("//a[contains(text(), 'Sign In')]")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.Id("email")).SendKeys("stefan1@gmail.com");
+            driver.FindElement(By.Id("pass")).SendKeys("Password123*");
+            driver.FindElement(By.XPath("(//button[@type='submit'])[2]")).Click();
+            Thread.Sleep(2000);
+
+            driver.FindElement(By.XPath("//a[@id='ui-id-8']")).Click();
+            driver.FindElement(By.XPath("//a[contains(text(), 'Fitness Equipment')]")).Click();
+            //Thread.Sleep(3000);
+            //driver.FindElement(By.CssSelector("product-item-info")).Click();
+            //Thread.Sleep(3000);
+            //driver.FindElement(By.XPath("(//button[@type='submit'])[2]")).Click();
         }
     }
 }
